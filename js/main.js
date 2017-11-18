667432a8f53103f64c710e4f39faa858df91a1bb
Jonathan Walker <jonathan.walker@ufl.edu>
2017-11-18 16:45:04 -0500

    Style paragraphs using jQuery

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var paragraphs = $('p');
+    paragraphs.css({
+        'color': '#555555',
+        'font-size': '1.2rem',
+        'font-family': 'georgia',
+        'line-height': '1.5rem'
+    });
+});
