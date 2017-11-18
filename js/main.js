6f509020d591208fd353397f45f4c1cefe1483fa
Jonathan Walker <jonathan.walker@ufl.edu>
2017-11-18 17:28:09 -0500

    Add document ready function

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
 $(document).ready(function () {
-    var paragraphs = $('p');
-    paragraphs.css({
-        'color': '#555555',
-        'font-size': '1.2rem',
-        'font-family': 'georgia',
-        'line-height': '1.5rem'
-    });
+    // More Code Goes Here.
 });
